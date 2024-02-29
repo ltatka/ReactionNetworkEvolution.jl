1fa0fd91bf899159481d5bdfd98f1f029a4a6a83
really-lilly <tatkalillian@gmail.com>
2024-02-29 07:19:23 -0800

    modify scripts to evaluate mutli-trial tests, but still errors

--- src/evaulate_trials.py
+++ src/evaulate_trials.py
@@ -12,9 +12,13 @@ def load_model(path):
     r = te.loada(astr)
     return r
 
+def writeout_model(r, path):
+    f = open(path, "w")
+    f.write(r.getCurrentAntimony())
+    f.close()
 def is_eigen_oscillator(r):
     try:
-        r.simulate(0, 1000, 5000)
+        r.steadyState()
         eigens = r.getFullEigenValues()
         for num in eigens:
             if num.real > 0 and num.imag != 0:
@@ -23,34 +27,109 @@ def is_eigen_oscillator(r):
     except Exception as e:
         return False
 
-def evaluate_trials_best_models(inputpath, outputpath, limit=None):
-    if not os.path.isdir(outputpath):
-        os.makedirs(outputpath)
+def evaluate_models_dir(path, outputpath, bestonly=False):
+    # This is for when you just have all the models in a directory
     models_evaluated = 0
     oscillators = 0
-    for file in os.listdir(inputpath):
-        fullpath = os.path.join(inputpath, file)
-        fullpath = os.path.join(fullpath, "final_models")
-        for model_file in os.listdir(fullpath):
+    for model_file in os.listdir(path):
+        if bestonly:
             if model_file.startswith("bestmodel"):
                 models_evaluated += 1
-                full_model_path = os.path.join(fullpath, model_file)
+                full_model_path = os.path.join(path, model_file)
                 r = load_model(full_model_path)
                 if is_eigen_oscillator(r):
                     oscillators += 1
                     outpath = os.path.join(outputpath, model_file)
+                    r.reset()
                     f = open(outpath, "w")
                     f.write(r.getCurrentAntimony())
                     f.close()
-                if limit and models_evaluated >= limit:
-                    print(f"Evaluated {models_evaluated} models and found {oscillators} oscillators")
-                    print(f"Success rate: {oscillators / models_evaluated}")
-                    return None
+        else:
+            models_evaluated += 1
+            full_model_path = os.path.join(path, model_file)
+            r = load_model(full_model_path)
+            if is_eigen_oscillator(r):
+                oscillators += 1
+                outpath = os.path.join(outputpath, model_file)
+                if os.path.isfile(outpath):
+                    print("duplicate model ID found")
+                    outpath += "_"
+                f = open(outpath, "w")
+                f.write(r.getCurrentAntimony())
+                f.close()
+    return models_evaluated, oscillators
+
+def evaluate_trials_best_models(inputpath, outputpath, childdir="final_models", limit=None, bestonly=False):
+    print("starting...")
+    if not os.path.isdir(outputpath):
+        os.makedirs(outputpath)
+    models_evaluated = 0
+    oscillators = 0
+    for file in os.listdir(inputpath): # Eg. 2024-02-24T12:03:00.332
+        fullpath = os.path.join(inputpath, file)
+        if childdir:
+            fullpath = os.path.join(fullpath, childdir)
+        total, oscs = evaluate_models_dir(fullpath, outputpath, bestonly=bestonly)
+        models_evaluated += total
+        oscillators += oscs
+        if models_evaluated % 50 == 0:
+            print(f"Evaluated: {models_evaluated}")
+        # for model_file in os.listdir(fullpath):
+        #     total, oscs = evaluate_models_dir(fullpath, outputpath, bestonly=bestonly)
+        #     models_evaluated += total
+        #     if models_evaluated % 50 == 0:
+        #         print(f"Evaluated {models_evaluated}")
+        #     oscillators += oscs
+
+            # if limit and models_evaluated >= limit:
+            #         print(f"Evaluated {models_evaluated} models and found {oscillators} oscillators")
+            #         print(f"Success rate: {oscillators / models_evaluated}")
+            #         return None
     print(f"Evaluated {models_evaluated} models and found {oscillators} oscillators")
     print(f"Success rate: {oscillators/models_evaluated}")
+
+def plot_trials(inputpath, rows, cols, childdir="final_models"):
+    limit = rows*cols
+    count = 0
+    fig, axs = plt.subplots(rows, cols)
+    models = []
+    for file in os.listdir(inputpath): # Eg. 2024-02-24T12:03:00.332
+        fullpath = os.path.join(inputpath, file)
+        if childdir:
+            fullpath = os.path.join(fullpath, childdir)
+        for model_file in os.listdir(fullpath):
+            if model_file.startswith("bestmodel"):
+                count += 1
+                models.append(load_model(os.path.join(fullpath, model_file)))
+                break
+        if count > limit:
+            break
+    idx = 0
+    for i in range(rows):
+        for j in range(cols):
+            try:
+                r = models[idx]
+                m = r.simulate()
+                axs[i,j].plot(m['time'], m[:,1:])
+                # a
+            except:
+                pass
+            idx += 1
+            if idx >= len(models):
+                break
+    plt.show()
 
 
 inputpath = "/home/hellsbells/Desktop/networkEv/Data/BenchmarkTests/1000_trials"
 outputpath = "/home/hellsbells/Desktop/networkEv/Data/BenchmarkTests/1000_trials_oscillators"
 
-evaluate_trials_best_models(inputpath, outputpath)
+plot_trials(outputpath, 1,11)
+
+# evaluate_trials_best_models(inputpath, outputpath, bestonly=True)
+
+# inpath = "/home/hellsbells/Desktop/networkEv/Data/oscillators_from_lab_update"
+# outpath = "/home/hellsbells/Desktop/networkEv/Data/oscillators_from_lab_update_true"
+#
+# # evaluate_trials_best_models(inpath, outpath, childdir=None)
+#
+# evaluate_models_dir(inpath, outpath, bestonly=False)
--- test_script.py
+++ test_script.py
@@ -6,13 +6,61 @@ import matplotlib.pyplot as plt
 import os
 import pandas as pd
 
+import math
+
+def plot_several_models_and_savefig(filepath, outpath, rows, cols):
+    total = len(os.listdir(filepath))
+    num_rounds = math.ceil(total/(rows*cols))
+    idx = 0
+    models = []
+    model_names = []
+    for dir in os.listdir(filepath):
+        parentdir = os.path.join(filepath, dir)
+        fullpath = os.path.join(parentdir, "final_models")
+        for model_file in os.listdir(fullpath):
+            if model_file.startswith("bestmodel"):
+                f = open(os.path.join(fullpath, model_file), "r")
+                astr = f.read()
+                f.close()
+                models.append(astr)
+                model_names.append(parentdir[-15:])
+    for rnd in range(num_rounds):
+
+        fig, axs = plt.subplots(rows, cols)
+        for i in range(rows):
+            if idx >= total:
+                plt.savefig(os.path.join(outpath, f"{rnd}"), dpi=5000)
+                plt.clf()
+                break
+            for j in range(cols):
+                r = te.loada(models[idx])
+                try:
+                    m = r.simulate(0)
+                    r.steadyState()
+                    axs[i, j].plot(m['time'], m[:, 1:])
+                    axs[i, j].set_title(f"{model_names[idx]}\n{has_oscillator_eigens(r.getFullEigenValues())}", size=8)
+                except:
+                    pass
+                idx += 1
+                if idx >= total:
+                    plt.savefig(os.path.join(outpath, f"{rnd}"), dpi=5000)
+                    plt.clf()
+                    break
+        plt.show()
+        plt.savefig(os.path.join(outpath, f"{rnd}"))
+        plt.clf()
+    print("done")
+
+
 
 def plot_several_models(filepath, rows, cols):
     fig, axs = plt.subplots(rows, cols)
     models = []
+    model_names = []
     cwd = os.getcwd()
     cwd = os.path.join(cwd, "src")
     for file in os.listdir(filepath):
+        model_names.append(file[6:])
         fullpath = os.path.join(filepath, file)
         fullpath = os.path.join(cwd, fullpath)
         f = open(fullpath, "r")
@@ -20,12 +68,27 @@ def plot_several_models(filepath, rows, cols):
         f.close()
     idx = 0
     for i in range(rows):
+        if idx >= len(model_names):
+            break
         for j in range(cols):
             r = te.loada(models[idx])
-            m = r.simulate(0,1,50)
-            axs[i,j].plot(m)
+            try:
+                m = r.simulate(0)
+                r.steadyState()
+                axs[i,j].plot(m['time'], m[:,1:])
+                axs[i,j].set_title(f"{model_names[idx]} {has_oscillator_eigens(r.getFullEigenValues())}", size=8)
+            except:
+                pass
             idx += 1
+            if idx >= len(models):
+                break
     plt.show()
+
+def has_oscillator_eigens(eigenvals):
+    for num in eigenvals:
+        if num.real > 0 and num.imag != 0:
+            return True
+    return False
 
 
 # path ="/home/hellsbells/Desktop/networkEv/src/final_models"
@@ -82,33 +145,156 @@ S2 = 9.0
 """
 
 evolvedmodel = """
-S0 + S0 -> S1 + S1; k1*S0*S0
-S0 + S0 -> S0 + S0; k2*S0*S0
-S1 + S2 -> S0 + S2; k3*S1*S2
-S2 -> S1 + S2; k4*S2
-S2 -> S2 + S2; k5*S2
-S1 -> S0 + S1; k7*S1
-S1 -> S2; k8*S1
-S1 + S2 -> S0; k9*S1*S2
-S2 -> S1; k10*S2
-k1 = 31.172546720935046
-k2 = 78.89088208128048
-k3 = 82.03285626562331
-k4 = 2.648349825474124
-k5 = 99.93579480894451
-k7 = 18.619724882494946
-k8 = 38.295673707771776
-k9 = 23.420218268236173
-k10 = 3.942945480210541
-S0 = 1.0
-S1 = 5.0
-S2 = 9.0
+// Created by libAntimony v2.12.0
+// Compartments and Species:
+species S0, S1, S2;
+
+// Reactions:
+_J0: S0 + S1 -> S1; k1*S0*S1;
+_J1: S1 + S2 -> S1 + S2; k2*S1*S2;
+_J2: S0 + S2 -> S1 + S1; k3*S0*S2;
+_J3: S0 -> S1 + S1; k4*S0;
+_J4: S0 -> S1; k5*S0;
+_J5: S1 + S2 -> S1 + S1; k6*S1*S2;
+_J6: S0 + S2 -> S0 + S1; k7*S0*S2;
+_J7: S2 -> S1 + S2; k8*S2;
+_J8: S2 -> S1 + S1; k9*S2;
+_J9: S2 -> S2 + S2; k10*S2;
+_J10: S2 + S2 -> S1; k11*S2*S2;
+_J11: S2 + S2 -> S0; k12*S2*S2;
+_J12: S2 -> S0 + S1; k13*S2;
+_J13: S1 + S1 -> S1 + S1; k14*S1*S1;
+_J14: S0 + S1 -> S0; k15*S0*S1;
+_J15: S2 + S2 -> S0 + S2; k16*S2*S2;
+_J16: S0 -> S0 + S1; k17*S0;
+_J17: S2 -> S0 + S2; k18*S2;
+_J18: S1 -> S0 + S1; k19*S1;
+_J19: S2 -> S2; k20*S2;
+_J20: S0 + S2 -> S1; k21*S0*S2;
+_J21: S2 -> S1; k22*S2;
+_J22: S2 -> S0; k23*S2;
+_J23: S1 + S2 -> S1; k24*S1*S2;
+
+// Species initializations:
+S0 = 1;
+S1 = 5;
+S2 = 9;
+
+// Variable initializations:
+k1 = 6.41994618410703;
+k2 = 0.0783887558898663;
+k3 = 11.6228695729583;
+k4 = 4.71537624468825;
+k5 = 12.2514262356933;
+k6 = 13.6248212876357;
+k7 = 30.1783681699379;
+k8 = 27.1515698331225;
+k9 = 7.98561518156907;
+k10 = 3.06230146453495;
+k11 = 4.79268604511165;
+k12 = 7.72034888874054;
+k13 = 11.229991807324;
+k14 = 7.36319961101907;
+k15 = 7.30870325529001;
+k16 = 7.99995231066672;
+k17 = 14.2199701328281;
+k18 = 4.61755712855557;
+k19 = 3.33735072232818;
+k20 = 4.29231822957825;
+k21 = 1.17627121315487;
+k22 = 3.52364195550499;
+k23 = 6.01086830929271;
+k24 = 1.78126089564575;
 """
 
 # compare_model_plots(model, evolvedmodel, simulation=[0, 10, 1000])
+#
+# r = te.loada(evolvedmodel)
+# r.simulate(0,1000,5000)
+# r.plot()
+# r.steadyState()
+# print(r.getFullEigenValues())
+# # print(r.getFullEigenValues())
 
-r = te.loada(evolvedmodel)
-r.simulate(0,1000,5000)
+input= "/home/hellsbells/Desktop/networkEv/Data/BenchmarkTests/1000_trials"
+output = "/home/hellsbells/Desktop/networkEv/Data/BenchmarkTests/1000_trials_oscillators"
+plot_several_models_and_savefig(input, output, 5, 5)
+
+# plot_several_models("/home/hellsbells/Desktop/networkEv/Data/BenchmarkTests/1000_trials_oscillators",2, 11)
+
+
+# path = "/home/hellsbells/Desktop/networkEv/Data/BenchmarkTests/1000_trials_oscillators"
+# modelname = "bestmodel_H4Pyg4D9SZ5i"
+#
+# model_path = os.path.join(path, modelname)
+#
+# f = open(model_path, "r")
+# astr = f.read()
+# f.close()
+# r = te.loada(astr)
+# r.simulate(0, 1, 100)
+# print(r.getFullEigenValues())
 # r.plot()
 
-print(r.getFullEigenValues())
+r = te.loada("""
+// Created by libAntimony v2.12.0
+// Compartments and Species:
+species S0, S2, S1;
+
+// Reactions:
+_J0: S0 -> S0 + S0; k1*S0;
+_J1: S0 + S2 -> S1 + S1; k2*S0*S2;
+_J2: S2 -> S1 + S2; k3*S2;
+_J3: S2 -> S2 + S2; k4*S2;
+_J4: S2 -> S1 + S1; k5*S2;
+_J5: S0 -> S0 + S2; k6*S0;
+_J6: S0 + S1 -> S0; k7*S0*S1;
+_J7: S1 -> S0 + S1; k8*S1;
+_J8: S2 -> S0 + S2; k9*S2;
+_J9: S1 -> S1 + S1; k10*S1;
+_J10: S0 + S2 -> S1; k11*S0*S2;
+
+// Species initializations:
+S0 = 1;
+S2 = 9;
+S1 = 2;
+
+// Variable initializations:
+k1 = 4.21910131567945;
+k2 = 11.712073055039;
+k3 = 11.0259246184376;
+k4 = 8.88687795278714;
+k5 = 2.40725961099904;
+k6 = 7.46683140994304;
+k7 = 2.91322140978341;
+k8 = 5.68283591047148;
+k9 = 7.63267449265815;
+k10 = 13.0555066455122;
+k11 = 8.77611525023671;
+
+// Other declarations:
+const k1, k2, k3, k4, k5, k6, k7, k8, k9, k10, k11;""")
+# r.simulate()
+# r.plot()
+#
+# r.steadyState()
+# print(r.getFullEigenValues())
+# r.reset()
+# fig, axs = plt.subplots(1, 3)
+#
+#
+# print(r.getFullEigenValues())
+# m = r.simulate(0,10,500)
+# axs[0].plot(m['time'], m[:,1:])
+# axs[0].set_title(f"{r.getFullEigenValues()}", size=8)
+#
+# r.reset()
+# m = r.simulate(0,1000,5000)
+# axs[1].plot(m['time'], m[:,1:])
+# axs[1].set_title(f"{r.getFullEigenValues()}", size=8)
+#
+# r.reset()
+# m = r.simulate(0,10000,50000)
+# axs[2].plot(m['time'], m[:,1:])
+# axs[2].set_title(f"{r.getFullEigenValues()}", size=8)
+# plt.show()
