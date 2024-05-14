cde9fe41ce0922c2e1ad04c3d4b27d5194ec14b4
really-lilly <tatkalillian@gmail.com>
2024-05-14 14:10:43 -0700

    reset quickstart script

--- quickstart.sh
+++ quickstart.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 counter=1 
 
-while [ $counter -le 7 ] 
+while [ $counter -le 5 ] 
 
 do  
 
     julia --project=. -e 'using Pkg; Pkg.instantiate()'  
 
-    julia --project=. run_evolution.jl --pathtosettings="/home/hellsbells/Desktop/evolution_output/no_speciation_new/settings.json" --outputpath="/home/hellsbells/Desktop/evolution_output/no_speciation_new/"
+    julia --project=. run_evolution.jl
 
     ((counter++)) 
 
